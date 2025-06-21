d218c3b97230f2393db4856365b687cef8e87ce4
ansh bansal <anshbansal811@gmail.com>
2025-06-21 19:29:28 +0530

    corrects

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const serverless = require("serverless-http");
-const { db } = require("../backend/db/db");
-const transactionsRouter = require("../backend/routes/transactions");
+const { db } = require("../db/db");
+const transactionsRouter = require("../routes/transactions");
 
 require("dotenv").config();
 
@@ -11,10 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to DB (ensure this only runs once per cold start)
-db();
+db(); // Connect to MongoDB
 
-// Mount your routes
 app.use("/api/v1", transactionsRouter);
 
 module.exports = app;
